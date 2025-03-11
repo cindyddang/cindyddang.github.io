0a101aebeead80e907c215d3e609aad415f97ca9
cindyddang <61917584+cindyddang@users.noreply.github.com>
2025-03-11 22:00:43 +0000

    Deploying to gh-pages from @ cindyddang/cindyddang.github.io@5e3639f3b81ff0ca0accc1ab9195b00591889f83 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "A PDF of my resume is on the right!",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -22,13 +22,6 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
-          },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
           },
         },{id: "post-a-post-with-image-galleries",
       
@@ -398,32 +391,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
+          section: "News",},{id: "projects-project-6",
           title: 'project 6',
           description: "a project with no image",
           section: "Projects",handler: () => {
@@ -443,6 +411,31 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-diabete-predictor",
+          title: 'Diabete Predictor',
+          description: "Data Science II, Fall 2024",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/diabetepreditor/";
+            },},{id: "projects-personal-financial-assistant",
+          title: 'Personal Financial Assistant',
+          description: "IB Computer Science, Spring 2021",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/financialassistant/";
+            },},{id: "projects-seattle-weather-prediction",
+          title: 'Seattle Weather Prediction',
+          description: "Introduction to Artifical Intelligent, Fall 2024",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/settleweatherprediction/";
+            },},{id: "projects-snake-game",
+          title: 'Snake Game',
+          description: "Personal project, Summer 2023",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/snakegame/";
+            },},{id: "projects-volunteer-matcher-webapp",
+          title: 'Volunteer Matcher Webapp',
+          description: "Software Design, Fall 2024",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/volunteermatcher/";
             },},{
         id: 'social-email',
         title: 'email',
